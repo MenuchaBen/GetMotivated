01c9cb806a7526fc9f93798934ef573e7abd074c
Menucha Benisti <menucha.benisti@mobileye.com>
2023-04-16 16:05:21 +0300

    full step

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+  cout << "\"We cannot solve problems with the kind of thinking we employed when we came up with them.\" — Albert Einstein" << endl;
+
+  return 0;
+}
